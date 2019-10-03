5ac36e28dd20e2438bc10d16688d6b1855fdc2e1
Lastor <lastoraya@gmail.com>
2019-10-03 18:41:07 +0800

    [初始] 建立專案 Server

---
+++ app.js
@@ -0,0 +1,69 @@
+// App Server
+
+// import package
+// ==============================
+
+// npm package
+const express = require('express')                      // framework
+const exphbs = require('express-handlebars')            // template engine
+
+const methodOverride = require('method-override')       // 控制 form method
+const session = require('express-session')              // session 輔助套件
+const passport = require('passport')                    // 處理 user authentication
+const flash = require('connect-flash')                  // 產生 flash message
+
+
+// 環境 setting
+// ==============================
+
+// 開發模式，使用環境變數
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
+const app = express()
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(flash())
+
+// 設定 session 與 passport
+app.use(session({
+  secret: '294104fu/ 20 ',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+// require('./config/passport.js')(passport)
+
+// 設定模板引擎
+const hbs = exphbs.create({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  // helpers: { ifEqual: require('./models/lib/helpers.js') }
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+
+
+// route 設定
+// ==============================
+
+app.get('/', (req, res) => {
+  res.send('Server Start.')
+})
+
+
+// start server
+// ==============================
+
+app.listen(app.get('port'), () => {
+  console.log(
+    'Node.js Server with Express is running.',
+    '\033[33m',
+    `=> http://localhost:${app.get('port')}`,
+    '\033[0m'
+  )
+})
